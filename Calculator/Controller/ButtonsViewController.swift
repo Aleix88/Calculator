da57ff84ce4cfa4b0a48c29fb5a6fdb0984574e9
Aleix Diaz Baggerman <aleixdiaz@MBP-de-Aleix.lan>
2020-09-02 14:50:12 +0200

    Add container views

---
+++ Calculator/Controller/ButtonsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ButtonsViewController.swift
+//  Calculator
+//
+//  Created by Aleix Diaz Baggerman on 02/09/2020.
+//  Copyright © 2020 Aleix Diaz Baggerman. All rights reserved.
+//
+
+import UIKit
+
+class ButtonsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ Calculator/Controller/DisplayViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DisplayViewController.swift
+//  Calculator
+//
+//  Created by Aleix Diaz Baggerman on 02/09/2020.
+//  Copyright © 2020 Aleix Diaz Baggerman. All rights reserved.
+//
+
+import UIKit
+
+class DisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
